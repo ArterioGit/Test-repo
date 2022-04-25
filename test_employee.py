72d890f6e067bb78c5a329e24c46bfacfb6adb05
ArterioGit <alessandro.bigucci@gmail.com>
2022-04-26 00:18:53 +0200

    changes for unit test

--- test_employee.py
+++ test_employee.py
@@ -4,11 +4,11 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
     def test_email(self):
-        emp_1 = Employee("Alessandro", "Bigucci", 50000)
-        emp_2 = Employee("Emanuela", "Bellone", 60000)
+        emp_1 = Employee("Alessandro", "Bigucci")
+        emp_2 = Employee("Emanuela", "Bellone")
 
         self.assertEqual(emp_1.email, "Alessandro.Bigucci@gmail.com")
-        self.assertEqual(emp_1.email, "Emanuela.Bellone@gmail.com")
+        self.assertEqual(emp_2.email, "Emanuela.Bellone@gmail.com")
 
 
 if __name__ == "__main__":
